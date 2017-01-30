2566f39a4380c79a6f993ac1b83f31ddc844d504
Norberto89 <norberto.hl@gmail.com>
2017-01-30 17:55:11 -0600

    Add files via upload

--- MaquinasTuring/MaqTur.cpp
+++ MaquinasTuring/MaqTur.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 #include <fstream>
 #include <list>
+#include <stdlib.h>
+#include <math.h>
 #include <string>
-#include <string.h>
 
 
 using namespace std;
 
 struct EstSym{
 	
-	const char* Estado;
-	const char* symbol;
+	string Estado;
+	string symbol;
 	bool operator==(const EstSym& rhs) const{
-        if(strcmp(Estado, rhs.Estado)==0 && strcmp(symbol,rhs.symbol)==0) return true;
+        if(Estado.compare(rhs.Estado)==0 && symbol.compare(rhs.symbol)==0) return true;
         else return false;
     }
 };
 
 struct Output{
-	const char* Estado;
-	const char* symbol;
-	const char* trans;
+	string Estado;
+	string symbol;
+	string trans;
 	bool operator==(const Output& rhs) const{
-        if(strcmp(Estado, rhs.Estado)==0 && strcmp(symbol,rhs.symbol)==0 &&  strcmp(trans,rhs.trans)==0) return true;
+        if(Estado.compare(rhs.Estado)==0 && symbol.compare(rhs.symbol)==0 &&  trans.compare(rhs.trans)==0) return true;
         else return false;
     }	
 };
@@ -33,35 +34,89 @@ struct Transicion{
 };
 
 
+bool find_L(list<Transicion> Trans, EstSym Elem, Output *OP){
+	list<Transicion>::iterator IniT=Trans.begin(),FinIt=Trans.end();
+	--FinIt;
+	int siz=ceil((double)Trans.size()/2.0);
+	for(int i=0;i<siz;i++){
+		if(Elem==IniT->input || Elem==FinIt->input){
+			*OP=(Elem==IniT->input)?IniT->outp:FinIt->outp;
+			return true;
+		}
+		--FinIt;
+		++IniT;
+	}
+	return false;
+}
 
 
 int main(int argv,char* argc[]){
 	const char* pth=argc[1];
 	string Cinta;
 	string temp;
-	EstSym Pair;
 	Output outpt;
 	Transicion Tr;
 	
 	list<Transicion> Trans;
+	
 	ifstream FileM(pth,ios::in);
 	
 	FileM>>Cinta;
-	while(!FileM.eof()){
-		FileM>>temp;
-		Tr.input.Estado=temp.c_str();
-		FileM>>temp;
-		Tr.input.symbol=temp.c_str();
-		FileM>>temp;
-		Tr.outp.Estado=temp.c_str();
-		FileM>>temp;
-		Tr.outp.symbol=temp.c_str();
-		FileM>>temp;
-		Tr.outp.trans=temp.c_str();
-		Trans.push_back(Tr);
-		cout<<temp<<endl;
-	}
+	cout<< "Transiciones: "<<endl;
 	
+	while(!FileM.eof()){
+		FileM>>Tr.input.Estado;
+		FileM>>Tr.input.symbol;
+		FileM>>Tr.outp.Estado;
+		FileM>>Tr.outp.symbol;
+		FileM>>Tr.outp.trans;
+		Trans.push_back(Tr);
+		cout << "(" << Tr.input.Estado<<","<<Tr.input.symbol<<")" <<"->("<<Tr.outp.Estado<<"," << Tr.outp.symbol<< "," << Tr.outp.trans<<")"<<endl;
+	}
+	cout<<"--------------------------------"<<endl<<endl<<endl;
+	
+	cout<<"Cinta: "<<endl;
+	cout<<Cinta<<endl<<endl;
+
+	cout << "Operaciones realizadas: "<<endl;
+	EstSym Pair;
+	
+	Pair.Estado="s";
+	Pair.symbol="init";
+	Output op;
+	bool fin=find_L(Trans, Pair, &op);
+	int count=0;
+	if(fin){
+		cout << "(" << Pair.Estado<<","<<Pair.symbol<<")" <<"->("<<op.Estado<<"," << op.symbol<< "," << op.trans<<")"<<endl;
+		while(op.Estado!= "stop" && op.Estado != "yes" && op.Estado != "no"){
+			Pair.Estado=op.Estado;
+			if(count>=0 && count<Cinta.size()){
+				Pair.symbol=Cinta[count];
+			}else if(count < 0){
+				Pair.symbol="init";
+			}else{
+				Pair.symbol="blank";
+			}
+			if(op.trans=="left"){
+				count--;
+			}else if(op.trans=="right"){
+				count++;
+			}
+			fin=find_L(Trans,Pair, &op);
+			if(!fin && (Pair.Estado== "stop" || Pair.Estado == "yes" || Pair.Estado == "no")){
+				cout<<"Estado no encontrado"<<endl;
+				cout<< "No puedo continuar"<<endl<<endl;
+				exit(0);
+			}
+			cout << "(" << Pair.Estado<<","<<Pair.symbol<<")" <<"->("<<op.Estado<<"," << op.symbol<< "," << op.trans<<")"<<endl;
+		}
+	}else{
+		cout<<"Estado no encontrado"<<endl;
+		cout << "No puedo continuar"<<endl<<endl;
+		exit(0);
+	}
+	cout<<endl;
+	cout<< op.Estado <<endl<<endl;
 	
 	return 0;
 }
