73763a26b828058e91b2fd9d5b727f890f8b12a8
Norberto89 <norberto.hl@gmail.com>
2017-01-30 15:28:04 -0600

    Add files via upload

--- MaquinasTuring/MaqTur.cpp
+++ MaquinasTuring/MaqTur.cpp
@@ -36,13 +36,29 @@ struct Transicion{
 
 
 int main(int argv,char* argc[]){
-	const char pth=argc[1];
-	const char* Cinta;
-	const char* temp;
+	const char* pth=argc[1];
+	string Cinta;
+	string temp;
+	EstSym Pair;
+	Output outpt;
+	Transicion Tr;
+	
 	list<Transicion> Trans;
 	ifstream FileM(pth,ios::in);
+	
+	FileM>>Cinta;
 	while(!FileM.eof()){
 		FileM>>temp;
+		Tr.input.Estado=temp.c_str();
+		FileM>>temp;
+		Tr.input.symbol=temp.c_str();
+		FileM>>temp;
+		Tr.outp.Estado=temp.c_str();
+		FileM>>temp;
+		Tr.outp.symbol=temp.c_str();
+		FileM>>temp;
+		Tr.outp.trans=temp.c_str();
+		Trans.push_back(Tr);
 		cout<<temp<<endl;
 	}
 	
